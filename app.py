88784c24fe8fa19961e0af7c8d7a927e27202189
Ankit Meher <meherankit717@gmail.com>
2025-06-02 23:33:51 +0530

    4. Web App has been created.

--- app.py
+++ app.py
@@ -13,14 +13,12 @@ def home():
     return render_template('index.html')    
 
 @app.route('/predict', methods=['POST'])
-def predict_api():
-    data = request.json['data']
-    print(data)
-    reshaped_data = np.array(list(data.values())).reshape(1, -1)
-    new_data = scaler.transform(reshaped_data)
-    output = model.predict(new_data)
-    print(output[0])
-    return jsonify(output[0])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1, -1))
+    prediction = model.predict(final_input)[0]
+    print(prediction)
+    return render_template('index.html', prediction_text=f'Predicted Value: {prediction:.2f}')
 
 if __name__ == "__main__":
     app.run(debug=True)
